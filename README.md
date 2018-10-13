2cf39df7ccb656d118f3a462a021bf07459dc8bd
benjamin l holsinger <37024167+bholsinger09@users.noreply.github.com>
2018-10-13 01:38:16 -0600

    Latest update
    - app.js updated to have all but on functionality ( alert indicating win and for user to press refresh to start over)
    - readme file updated

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
-frontend-nanodegree-arcade-game
-===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+  ### frontend-nanodegree-arcade-game
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+   #####  Game files needed 
+   ```
+  * app.js
+  * engine.js
+  * resource.js 
+  * index.html
+  * referance github:  https://github.com/bholsinger09/classic-arcade-game-udacity-project-
+   ```
+   
+   ##### Browser supported
+- edge 17 , 18 , 19
+
+- Firefox 60 , 61 , 62 , 63 , 64 
+
+- Crome 68 , 69 , 70 , 71 
+
+- Safaro 11.1 , 12 , TP 
+
+- WKWebView
+
+- Echo js
+
+- Kinoma XS6
+
+- Node 6.5 - 9
+   
+  #####  Game consists of
+   - Playing platform:
+      - water at the top
+      - road in the middle
+      - land at the bottom
+   - Enemy Bugs along the road
+   - Moveable Player that starts on land 
+     
+    
+   
+   
+   
+   #####  How to play the game 
+    
+     1.  Player opens html file in browser
+     2.  Player will be starting at the bottom where the grass is.
+     3.  Player will need to use keyboad arrow keys to move character 
+     3.  Goal is for the character to move to the water without hitting the bugs 
+     4.  When player gets to the water then game is won. 
+     
+
--- js/app.js
+++ js/app.js
@@ -1,36 +1,144 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/*items changed from orginal code :
+changed all syntex from ecmascript 5 to 6
+used class and arrow funtions 
+built enemy object (which has x, y, and speed)
+created update method to enemy object
+enemy object method updates speed of enamy and checks for collision with player
+created player object (which has x and y properties )
+added keyPress method to handle player going up, down, left, and right
+created variable for allEnemies with empty array
+stored enemy location in const variable
+created foreach method for enemies 
+instantiate player object with starting position 
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+*/
+
+// Enemies our player must avoid
+class Enemy {
+    constructor(x, y, speed) {
+
+        // variables for object to track position of enemy and speed
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+
+        // image of enemy added to object
+        this.sprite = 'images/enemy-bug.png';
+    }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+  // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+
+        // Multiplies the speed by the dt parameter on the x axis
+        this.x += this.speed * dt;
+
+        // Once enemies are off the canvas, they reappear randomly with different speeds
+        if (this.x > 510) {
+            this.x = -30;
+            this.speed = 100 + Math.floor(Math.random() * 200);
+        };
+  
+            
+     // Checks for collisions between the player and the enemies
+        if (player.x < this.x + 80 &&
+            player.x + 80 > this.x &&
+            player.y < this.y + 40 &&
+            40 + player.y > this.y) {
+            player.x = 190;
+            player.y = 385;
+        };
+    }
+
+  // Renders the enemy into the game (required method)
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 
+
+
+
+
+// Player class object with x and y properties for location of player
+class Player {
+    constructor(x, y) {
+
+        // Variables for the player to move along x and y axis 
+        this.x = x;
+        this.y = y;
+
+        //The image of the player boy 
+        this.player = 'images/char-boy.png';
+    }
+  
+   update(dt) {
+
+    }
+  
+// Renders the image of player
+    render() {
+        ctx.drawImage(Resources.get(this.player), this.x, this.y);
+    }
+
+
+    handleInput(keyPress) {
+     if (keyPress == 'left' && this.x > 0) {
+            this.x -= 102;
+        };
+
+      if (keyPress == 'right' && this.x < 385) {
+            this.x += 102;
+        };
+
+        // Enables player/user to move on the y axis by 80 upward on keypress
+
+        if (keyPress == 'up' && this.y > 0) {
+            this.y -= 80;
+        };
+
+        // Enables player/user  to move on the y axis by 80 downward on keypress
+        // Also enables player/user not to go off the game tiles on the bottom side
+
+        if (keyPress == 'down' && this.y < 385) {
+            this.y += 80;
+        };
+
+
+    
+     if (this.y < 0) {
+            setTimeout(() => {
+                this.x = 190;
+                this.y = 385;
+            }, 800);
+        };
+  
+  
+  }
+
+}
+
+
+
+var allEnemies = [];
+// Placed all enemy objects in an array called allEnemies
+
+
+// Location of the 3 enemies on the y axis 
+const enemyLocation = [60, 145, 225];
+
+enemyLocation.forEach(locationY => {
+    enemy = new Enemy(0, locationY, 250);
+    allEnemies.push(enemy);
+});
+
+// instantiate player object with start position
+var player = new Player(190, 385);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -44,3 +152,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
